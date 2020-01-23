2f98a8d8310998215793d29da34253bb9523d7ee
Drew42069 <60240750+Drew42069@users.noreply.github.com>
2020-01-23 13:59:39 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".math").click(function() {
+    $(".Math").append("<br>functions! ");
+});
+
+$("img.coding").click(function() {
+    $(".coding").append("<br>HTML! CSS! jQuery! ");
+});
+
+$("img.history").click(function() {
+    $(".history").append("<br>Native Americans! ");
+});
+
+$("img.english").click(function() {
+    $(".english").append("<br>how to write an essay! ");
+});
